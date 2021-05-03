22275801dafdb65229689a1ec86d1fe5e764a5d6
Carlos Baquero <karlosmario0123@gmail.com>
2021-05-03 09:05:35 -0500

    Clases Base

---
+++ ventas.infrastructure/Base/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Banco.Domain.Contracts;
+using Banco.Infrastructure.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banco.Infrastructure.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbContext _context;
+        public UnitOfWork(IDbContext context) => _context = context;
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        
+    }
+}
