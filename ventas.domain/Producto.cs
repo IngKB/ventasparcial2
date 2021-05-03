ad71f8c7b7e83671dc4e0c3dcf2da7537279e268
Carlos Baquero <karlosmario0123@gmail.com>
2021-05-03 09:25:09 -0500

    agregado el context

--- ventas.domain/Producto.cs
+++ ventas.domain/Producto.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ventas.domain.Base;
 
 namespace ventas.domain
 {
-    public abstract class Producto
+    public abstract class Producto: Entity<int>
     {
-        private string Id { get; set; }
+        public string Codigo { get; private set; }
         public string Nombre { get; protected set; }
         protected decimal Costo { get; set; }
         public decimal Precio { get; protected set; }
 
 
-        public Producto(string id, string nombre, decimal costo, decimal precio)
+        public Producto(string codigo, string nombre, decimal costo, decimal precio)
         {
-            Id = id;
+            Codigo = codigo;
             Nombre = nombre;
             Costo = costo;
             Precio = precio;
